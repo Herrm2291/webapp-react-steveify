7548c72ed01abdf07e528cc010c65914ee8932e5
Steve Herrmann <37193114+Herrm2291@users.noreply.github.com>
2018-05-12 19:35:53 -0400

    created Track component

---
+++ src/Components/Track/Track.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './Track.css';
+
+class TrackList extends Component {
+  render() {
+    return (
+      <div className="Track">
+        <div className="Track-information">
+          <h3><!-- track name will go here --></h3>
+          <p><!-- track artist will go here--> | <!-- track album will go here --></p>
+        </div>
+        <a className="Track-action"><!-- + or - will go here --></a>
+      </div>
+    );
+  }
+
+  /* ternary operator used to reduce code of this if/else statement which displays a + or a - dependant on the boolean return value from this.props.isRemoval */
+  renderAction() {
+    return <a className="Track-action">{this.props.isRemoval ? '-' : '+'}</a>;
+  }
+
+export default Track;
+
