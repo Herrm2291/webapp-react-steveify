70cf9ab55ddab5c199afc2b481ff576c5de6140a
Steve Herrmann <37193114+Herrm2291@users.noreply.github.com>
2018-05-12 20:14:21 -0400

    bug fixes and image changes

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <h1>Steveify</h1>
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
