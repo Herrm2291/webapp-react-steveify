0748202bc79ef4f95df13ab918065f9e95c431d1
Steve Herrmann <37193114+Herrm2291@users.noreply.github.com>
2018-05-13 10:02:32 -0400

    syntax fixes

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -8,7 +8,7 @@ class TrackList extends Component {
       <div className="TrackList">
         {
           this.props.tracks.map(track => {
-            return <Track key={track.id} />
+            return <Track track={track} key={track.id} />
           })
         }
       </div>
