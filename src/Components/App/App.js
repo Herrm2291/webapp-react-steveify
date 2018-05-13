02c2dcd70bf359c42f386e37f9f8a7cbeadc7ea7
Steve Herrmann <37193114+Herrm2291@users.noreply.github.com>
2018-05-13 09:32:21 -0400

    added constructor function to the App component, setting a hard-coded initial value for this.state.searchResults, passed state of teh App components searchResults to the SearchResults component and passed the search results from the SearchResults component to the TrackList component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,6 +5,18 @@ import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [
+        'name': '',
+        'artist': '',
+        'album': '',
+        'id': ''
+      ]
+    };
+  }
+
   render() {
     return (
       <div>
@@ -12,7 +24,7 @@ class App extends Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-          <SearchResults />
+          <SearchResults searchResults={this.state.searchResults}/>
           <Playlist />
           </div>
         </div>
